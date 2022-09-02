db5ece433d165507a192fa4b792759261d97278f
tovehydmark <tovehydmark@gmail.com>
2022-09-02 14:41:03 +0200

    iubhi

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ArtGallery } from "./components/artGallery/ArtGallery";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ArtGallery } from './components/artGallery/ArtGallery';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}></Route>
-            <Route path="/artGallery" element={<ArtGallery></ArtGallery>}></Route>
-        </Routes>
-    </BrowserRouter>
-  
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/artGallery" element={<ArtGallery></ArtGallery>}></Route>
+    </Routes>
+  </HashRouter>
+
   // </React.StrictMode>
 );
